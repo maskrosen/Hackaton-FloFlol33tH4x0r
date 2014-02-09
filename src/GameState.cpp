e9d539a5e547001161c0901113e7bf96ca6b2d91
Florian Minges <florianminges@gmail.com>
2014-02-09 15:32:31 +0100

    Added GameState
    initial implementation. Go get them, Grubla.

---
+++ src/GameState.cpp
@@ -0,0 +1,21 @@
+
+#include "GameState.h"
+
+
+
+GameState::GameState(void) {
+
+}
+
+void GameState::initFood(void) {
+	int score = 10;
+	int radius = 10;
+	float x = 10.f;
+	float y = 20.f;
+	
+	for (int i = 0; i < 100; i++) {
+		GameState::food[i] = FoodItem::FoodItem(100,20,x,y);
+	}
+}
+
+
