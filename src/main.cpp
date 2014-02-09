81dc3f1bf7fb704b180bd827e357bea703d6b216
AftonTroll <filbry_92@hotmail.com>
2014-02-09 11:53:45 +0100

    Time is imortant man... it's impoartant....
    MY HANDS ARE TYPING WORDS /Plankton

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <time.h>
+using namespace std;
 
 int main()
 {
+	time_t startTime;
+	time_t nowTime;
+	time(&startTime);
     sf::RenderWindow window(sf::VideoMode(800, 600), "Hackaton project FloFloL33tH4x0r");
     sf::CircleShape shape(150.f);
     shape.setFillColor(sf::Color::Blue);
@@ -19,6 +25,9 @@ int main()
         window.clear();
         window.draw(shape);
         window.display();
+		time(&nowTime);
+		double seconds = difftime(nowTime,startTime);
+		cout << seconds << endl;
     }
 
     return 0;
