849a757f58f0c912e2a6dac6c837c925eff32af5
Björn Mattsson <bjorn.pm@plankter.se>
2014-02-12 11:01:31 +0100

    Changed objects to pointers and moved event code to function

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,32 @@
 
 using namespace std;
 
+
+sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(800, 600), "Hackaton project FloFloL33tH4x0r");
+
+Pacman* pacman = new Pacman(100.f, 200.f);
+PacmanInput* pacmanInput = new PacmanInput(pacman);
+Ghost* ghost = new Ghost(30.f, 40.f, 200.f, 300.f, sf::Color::Red);
+
+void handleEvents()
+{
+	sf::Event event;
+	while (window->pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			window->close();
+			break;
+
+		case sf::Event::KeyPressed:
+			pacmanInput->ProcessEvent(event);
+			break;
+		}
+
+	}
+}
+
 int main()
 {
 	/* Variables gg */
@@ -16,20 +42,11 @@ int main()
 	float mouthTimer = 0;
 	clock_t tNow;
 	clock_t tLast = 0;
+
 	Map map;
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Hackaton project FloFloL33tH4x0r");
-    sf::CircleShape shape(150.f);
-    shape.setFillColor(sf::Color::Blue);
-	
-	Pacman* pacman = new Pacman(100.f, 200.f);
-	PacmanInput* pacmanInput = new PacmanInput(pacman);
-
-	Ghost ghost(30.f, 40.f, 200.f, 300.f, sf::Color::Red);
-
-
 	//Zhe amaaaaazing Game Loop
-    while (window.isOpen())
+    while (window->isOpen())
     {
 		//Update time
 		tNow = clock();
@@ -37,56 +54,42 @@ int main()
 		tLast = tNow;
 		printf ("%f sec, %d clicks.\n",((float)tNow)/CLOCKS_PER_SEC,tNow);
 
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            switch (event.type)
-			{
-				case sf::Event::Closed:
-					window.close();
-					break;
-
-				case sf::Event::KeyPressed:
-					pacmanInput->ProcessEvent(event);
-					break;
-			}
-
-        }
+		handleEvents();
 
 		pacman->update((float)tDiff);
 
 		/* Draw Stuff */
-        window.clear();
+        window->clear();
 
 		// window.draw(shape);
 		for (int i = 0; i< map.mapSize(); i++) {
 			for (int j = 0; j< map.mapSize(); j++) {
-				window.draw(map.getShape(i,j));
+				window->draw(map.getShape(i,j));
 			}
 		}
-		window.draw(pacman->getBody());
+		window->draw(pacman->getBody());
 		
 		mouthTimer+=tDiff;
 		if(mouthTimer>=0.25){
-			window.draw(pacman->getMouth());
+			window->draw(pacman->getMouth());
 		}
 		if(mouthTimer>=0.5){			
 			mouthTimer=0;
 		}
 
-		window.draw(ghost.getBody());
-		window.draw(ghost.getHead());
-		window.draw(ghost.getLeg1());
-		window.draw(ghost.getLeg2());
-		window.draw(ghost.getLeg3());
-		window.draw(ghost.getLeg4());
-		window.draw(ghost.getEye1());
-		window.draw(ghost.getEye2());
-		window.draw(ghost.getEyeP1());
-		window.draw(ghost.getEyeP2());
+		window->draw(ghost->getBody());
+		window->draw(ghost->getHead());
+		window->draw(ghost->getLeg1());
+		window->draw(ghost->getLeg2());
+		window->draw(ghost->getLeg3());
+		window->draw(ghost->getLeg4());
+		window->draw(ghost->getEye1());
+		window->draw(ghost->getEye2());
+		window->draw(ghost->getEyeP1());
+		window->draw(ghost->getEyeP2());
 
 
-        window.display();
+        window->display();
     }
 
     return 0;
